92824c18ce8bca22884311ff2d6d9ae3edbda2f2
Kakeru Fukuda <stand.by.we@gmail.com>
2019-04-10 21:37:40 +0900

    ガワ作成

---
+++ SwiftChainingAssertion/SwiftChainingAssertion.h
@@ -0,0 +1,19 @@
+//
+//  SwiftChainingAssertion.h
+//  SwiftChainingAssertion
+//
+//  Created by 福田走 on 2019/04/10.
+//  Copyright © 2019 福田走. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftChainingAssertion.
+FOUNDATION_EXPORT double SwiftChainingAssertionVersionNumber;
+
+//! Project version string for SwiftChainingAssertion.
+FOUNDATION_EXPORT const unsigned char SwiftChainingAssertionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftChainingAssertion/PublicHeader.h>
+
+
---
+++ SwiftChainingAssertion/SwiftChainingAssertion.swift
@@ -0,0 +1,16 @@
+//
+//  SwiftChainingAssertion.swift
+//  SwiftChainingAssertion
+//
+//  Created by 福田走 on 2019/04/10.
+//  Copyright © 2019 福田走. All rights reserved.
+//
+
+import Foundation
+import XCTest
+
+extension Equatable {
+    public func `is`(_ value: Self.Type) {
+        
+    }
+}
